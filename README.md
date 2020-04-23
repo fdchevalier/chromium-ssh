05e428421bec2e33cdc3a29b33983ee91a5a77bf
fdchevalier <fcheval@txbiomed.org>
2020-04-22 23:02:32 -0500

    chromium-ssh - v0.0

---
+++ README.md
@@ -0,0 +1,34 @@
+# chromium-ssh
+
+This script creates an SSH tunnel and start the Chromium browser with the right parameters to use it.
+
+## Prerequisites
+
+To run the script properly, you need to install `chromium-browser` if not already present and to have an SSH server to connect to.
+
+## Files
+
+The files available are:
+* `chromium-ssh.sh`: the script which creates an SSH tunnel and start chromiu
+* `icon/chromium-ssh.png`: a dedicated icon derived from the chromium browser icon
+
+
+## Installation
+
+To download the latest version of the files:
+```
+git clone https://github.com/fdchevalier/
+```
+
+For convenience, the script should be accessible system-wide by either including the folder in your `$PATH` or by moving the script in a folder present in your path (e.g. `$HOME/local/bin/`).
+
+To install the icon (optional):
+```bash
+[[ -d $HOME/.icons ]] || mkdir "$HOME/.icons"
+cp icon/chromium-ssh.png "$HOME/.icons/"
+```
+
+## Usage
+
+A summary of available options can be obtained using `./chromium-ssh -h`.
+
---
+++ chromium-ssh.sh
@@ -0,0 +1,228 @@
+#!/bin/bash
+# Title: chromium-ssh.sh
+# Version: 0.0
+# Author: Frédéric CHEVALIER <fcheval@txbiomed.org>
+# Created in: 2015-03-07
+# Modified in:
+# Licence : GPL v3
+
+
+
+#======#
+# Aims #
+#======#
+
+aim="Set a SSH tunnel for chromium."
+
+
+
+#==========#
+# Versions #
+#==========#
+
+# v0.0 - 2015-03-07: creation
+
+
+
+#===========#
+# Functions #
+#===========#
+
+# Usage message
+function usage {
+    echo -e "
+    \e[32m ${0##*/} \e[00m -u|--usr username -s|--svr server -p|--port integer -l|--log path -h|--help
+
+Aim=$aim
+
+Options:
+    -u, --usr       user name required to connect the ssh server
+    -s, --svr       address of the ssh server to set the ssh tunnel up
+    -p, --port      local port for ssh tunnel [default: 8080]
+    -l, --log       path to the log file [default: /tmp/${0##*/}.log]
+    -h, --help      this message
+"
+}
+
+
+# For info, warning, and error functions, look at http://stackoverflow.com/a/911213 to see what descriptor to test (0 if it has to be run in graphic mode, 1 if it has to be run in a terminal)
+
+# Info message
+function info {
+    if [[ -t 0 ]]
+    then
+        echo -e "\e[32mInfo:\e[00m $1"
+    else
+        notify-send $myicon -h string:x-canonical-append:allowed "${0##*/} - Info: $1"
+    fi
+}
+
+
+# Warning message
+function warning {
+    if [[ -t 0 ]]
+    then
+        echo -e "\e[33mWarning:\e[00m $1"
+    else
+        notify-send $myicon -h string:x-canonical-append:allowed "${0##*/}" "Warning: $1"
+    fi
+}
+
+
+# Error message
+## usage: error message [exit_code]
+## exit code optional
+function error {
+    if [[ -t 0 ]]
+    then
+        echo -e "\e[31mError:\e[00m $1"
+    else
+        notify-send $myicon -h string:x-canonical-append:allowed "${0##*/}" "Error: $1"
+    fi
+
+    if [[ -n $2 ]]
+    then
+        exit $2
+    fi
+}
+
+
+# Dependency test
+function test_dep {
+    which $1 &> /dev/null
+    if [[ $? != 0 ]]
+    then
+        error "Package $1 is needed. Exiting..." 1
+    fi
+}
+
+
+
+#==============#
+# Dependencies #
+#==============#
+
+test_dep notify-send
+test_dep chromium-browser
+
+
+
+#===========#
+# Variables #
+#===========#
+
+# Load variables
+while [[ $# -gt 0 ]]
+do
+    case $1 in
+        -u|--usr    ) myuser="$2" ; shift 2 ;;
+        -s|--svr    ) myserver="$2" ; shift 2 ;;
+        -p|--port   ) myport="$2" ; shift 2 ;;
+        -l|--log    ) mylog="$2" ; shift 2 ;;
+        -h|--help   ) usage ; exit 0 ;;
+        *           ) error "Invalid option: $1" 1 ;;
+    esac
+done
+
+
+# Icon for notification
+if [[ -f "$HOME/.icons/chromium-ssh.png" ]]
+then
+    myicon="-i $HOME/.icons/chromium-ssh.png"
+else
+    myicon="-i chromium-browser"
+fi
+
+
+# Check for mendatory options
+if [[ -z "$myuser" ]]
+then
+    error "username missing for ssh connection." 1
+fi
+
+if [[ -z "$myserver" ]]
+then
+    error "server address missing for ssh connection." 1
+fi
+
+myssh_add="$myuser@$myserver"
+
+
+# Set default values if nothing specify
+if [[ -z "$port" ]]
+then
+    myport=8080
+fi
+
+if [[ -z "$log" ]]
+then
+    mylog="/tmp/${0##*/}.log"
+fi
+
+
+
+#============#
+# Processing #
+#============#
+
+
+#-----------------------#
+# Set up the SSH tunnel #
+#-----------------------#
+## source: http://stackoverflow.com/a/15198031
+
+# Info message
+date > "$mylog"
+mymsg="SSH tunnel initialization..."
+info "$mymsg"
+echo -e "\n$mymsg\n" >> "$mylog"
+
+# SSH tunnel
+ssh -M -S my-ctrl-socket -C2fnNT -D $myport "$myssh_add" &>> "$mylog"
+
+if [[ $? != 0 ]]
+then
+    error "SSH tunnel cannot be set up. See $mylog" 1
+fi
+
+# Check socket
+ssh -S my-ctrl-socket -O check "$myssh_add" &>> "$mylog"
+
+# Environmental parameters
+SOCKS_SERVER="localhost:$myport"
+SOCKS_VERSION=5
+
+
+#-----------------#
+# Start Chromium  #
+#-----------------#
+
+# Info message
+mymsg="Chromium starting..."
+info "$mymsg"
+echo -e "\n\n$mymsg\n" >> "$mylog"
+
+# Start Chromium
+## source: http://www.chromium.org/developers/design-documents/network-stack/socks-proxy
+chromium-browser --proxy-server="socks://localhost:$myport" --host-resolver-rules="MAP * 0.0.0.0 , EXCLUDE localhost" &>> "$mylog"
+
+if [[ $? != 0 ]]
+then
+    error "Chromium closed unexpectedly. See $mylog" 1
+fi
+
+
+#-----------------#
+# Close SSH tunel #
+#-----------------#
+
+# Info message
+mymsg="SSH tunel closing..."
+info "$mymsg"
+echo -e "\n\n$mymsg\n" >> "$mylog"
+
+# Close SSH unnel
+ssh -S my-ctrl-socket -O exit "$myssh_add" &>> "$mylog"
+
+
+exit 0
